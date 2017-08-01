d14ffa7b20a26c7fa97452183c2de8031a4c7b80
EarthenSky <gabegames27@gmail.com>
2017-08-01 00:04:31 -0700

    added an audio track

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
-var speech = document.getElementById("txt");
-var face = document.getElementById("face");
-var ui = document.getElementById("ui");
+var speech = document.getElementById('txt');
+var face = document.getElementById('face');
+var ui = document.getElementById('ui');
+
+var track1 = new Audio('js/jsResources/146Endless.wav');
 
 function start ()  //this is called on load;
 {
@@ -10,37 +12,37 @@ function start ()  //this is called on load;
 
 function dialogue0() {
   face.innerHTML = "[o_o]";
-  speech.innerHTML += "<br> Hey you!";
+  speech.innerHTML += "<br> > Hey you!";
   setTimeout(dialogue1, 2000);
 }
 
 function dialogue1() {
   face.innerHTML = "[>_<]";
-  speech.innerHTML += "<br> Look what you've done!";
+  speech.innerHTML += "<br> > Look what you've done!";
   setTimeout(dialogue2, 4000);
 }
 
 function dialogue2() {
   face.innerHTML = "[-_-]";
-  speech.innerHTML += "<br> Sigh...";
+  speech.innerHTML += "<br> > Sigh...";
   setTimeout(dialogue3, 4000);
 }
 
 function dialogue3() {
   face.innerHTML = "[-.-]";
-  speech.innerHTML += "<br> I'm going to go try and fix this,";
+  speech.innerHTML += "<br> > I'm going to go try and fix this,";
   setTimeout(dialogue4, 2000);
 }
 
 function dialogue4() {
   face.innerHTML = "[-.-]";
-  speech.innerHTML += "<br> Dont break anything ok?";
-  setTimeout(addButton, 2000, "ok?", "dialogue5()");
+  speech.innerHTML += "<br> > Dont break anything ok?";
+  setTimeout(addbutton, 2000, "ok?", "dialogue5()");
 }
 
 function dialogue5() {
   face.innerHTML = "[-w-]";
-  speech.innerHTML = "Great.";
+  speech.innerHTML = " > Great.";
   ui.innerHTML = "";
   setTimeout(dialogue6, 2000);
 }
@@ -48,9 +50,15 @@ function dialogue5() {
 function dialogue6() {
   face.innerHTML = "[___]"
   speech.innerHTML = ""
+
+  //start idle audio.
+  track1.play();
+  track1.loop = true;
+  track1.volume = 0.1;
+
   //setTimeout(dialogue7, 2000); ???
 }
 
-function addButton(txt, tocall) {
+function addbutton(txt, tocall) {
   ui.innerHTML = '<button class="inverted" onclick="' + tocall + '">' + txt + '</button>'
 }
