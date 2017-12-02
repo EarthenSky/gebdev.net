1176d2529955b2dec1c3fd46e5e517f927fdf6f2
EarthenSky <gabegames27@gmail.com>
2017-12-02 12:59:33 -0800

    deleted deprecated website

--- stop_website_from_exploding/js/mathFunctions.js
+++
@@ -1,5 +0,0 @@
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The minimum is inclusive maximum is exclusive and the
-}
