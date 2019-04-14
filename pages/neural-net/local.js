2aed82322149223ec8a0343b448c15bd2f74eafb
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2019-04-14 10:53:15 -0700

    a bit of lead up to neural networks

--- pages/neural-net/local.js
+++ pages/neural-net/local.js
@@ -1,1 +1,13 @@
-//hi
+function main() {
+    // Load text file.
+    // Setup network.
+    // Train network.
+}
+
+let idStack = [];
+let INPUT_NODES = ;
+let HIDDEN_LAYER_DEPTH = 5;
+let HIDDEN_LAYER_SIZE = 5;
+function startTraining() {
+
+}
