cbff86320cafaec678ae2c6cf01b2ea36486a972
Gabe <24978329+EarthenSky@users.noreply.github.com>
2022-01-06 03:25:57 -0800

    initial version of new site

---
+++ scripts/parse.js
@@ -0,0 +1,10 @@
+// this file parses blog posts from a custom markdown into html
+
+function parse(file_address, target_div) {
+    // read file to str
+    
+    // parse string
+
+    // add to div
+
+}
